807d467b4ffde34c52df56aa11e2733fbc4cb13a
Denison Kolling <denisonkolling@gmail.com>
2023-11-16 15:46:04 -0300

    feat: add authentication service

---
+++ src/main/java/com/fastfood/service/AuthenticationService.java
@@ -0,0 +1,7 @@
+package com.fastfood.service;
+
+public interface AuthenticationService {
+
+    String authentication(String email, String password);
+
+}
---
+++ src/main/java/com/fastfood/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,38 @@
+package com.fastfood.service.impl;
+
+import com.fastfood.model.User;
+import com.fastfood.service.AuthenticationService;
+import com.fastfood.service.TokenService;
+import com.fastfood.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public String authentication(String email, String password) {
+        try {
+            var authToken = new UsernamePasswordAuthenticationToken(email, password);
+            Authentication authenticate = authenticationManager.authenticate(authToken);
+            var user  = (User) authenticate.getPrincipal();
+            String token = tokenService.createToken(user);
+            return token;
+        } catch (AuthenticationException e) {
+            throw new IllegalArgumentException("Invalid User or Password");
+        }
+    }
+}
